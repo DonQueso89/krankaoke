23881715ccd78fd80712dc03989e7899244e5160
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2020-02-28 11:38:57 +0100

    fix api token swagger spec

--- krankaoke_backend/krankaoke_backend/urls.py
+++ krankaoke_backend/krankaoke_backend/urls.py
@@ -25,19 +25,29 @@ from krankaokes import views as krankaoke_views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers
+
+
+class APITokenSerializer(serializers.Serializer):
+    """
+    just for the Swagger spec
+    """
+
+    username = serializers.CharField()
+    password = serializers.CharField()
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Krankaoke API",
-      default_version='v1',
-      description="Generic backend for Krankaoke apps",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="kg.v.ekeren@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Krankaoke API",
+        default_version="v1",
+        description="Generic backend for Krankaoke apps",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="kg.v.ekeren@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 router = routers.DefaultRouter()
@@ -46,16 +56,17 @@ router.register(r"users", user_views.CreateUserViewSet)
 router.register(r"krankaokes", krankaoke_views.KrankaokeViewSet, basename="krankaokes")
 
 
-api_token_params = [
-    openapi.Parameter('username', openapi.IN_BODY, type=openapi.TYPE_STRING),
-    openapi.Parameter('password', openapi.IN_BODY, type=openapi.TYPE_STRING),
-]
-
-api_token_view = swagger_auto_schema(method='post', manual_parameters=api_token_params)(drf_views.obtain_auth_token)
+api_token_view = swagger_auto_schema(method="post", request_body=APITokenSerializer)(
+    drf_views.obtain_auth_token
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
     path("api_token/", api_token_view),
-    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
