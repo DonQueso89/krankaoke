928764e252f0a071ee1911c49eb4e64d63007776
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2020-03-15 18:59:44 +0100

    validation for timings

--- krankaoke_backend/krankaokes/serializers.py
+++ krankaoke_backend/krankaokes/serializers.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import serializers
 
 from krankaokes.models import Krankaoke
@@ -6,12 +7,28 @@ from krankaokes.models import Krankaoke
 class ListKrankaokeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Krankaoke
-        fields = ['id', 'artist', 'title', 'user']
+        fields = ["id", "artist", "title", "user"]
+
+
+def validate_timings(value):
+    value = json.loads(value)
+    value = sorted([(w, s, e) for w, (s, e) in value.items()], key=lambda k: k[1])
+
+    last_word, last_end = 0, -1
+    for word, start, end in value:
+        if start >= end:
+            raise serializers.ValidationError(
+                f"end time for '{word}' must be smaller than start"
+            )
+        if last_end >= start:
+            raise serializers.ValidationError(
+                f"{word} and {last_word} overlap with eachother"
+            )
 
 
 class KrankaokeSerializer(serializers.ModelSerializer):
-    timings = serializers.JSONField()
+    timings = serializers.JSONField(validators=[validate_timings])
 
     class Meta:
         model = Krankaoke
-        fields = ['artist', 'title', 'audio', 'user', 'timings']
+        fields = ["artist", "title", "audio", "user", "timings"]
